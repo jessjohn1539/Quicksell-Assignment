dbfc5b78cbecbd9717d016caf9dbfcbef1a09c11
Jess John <jessjohn1539@gmail.com>
2024-11-04 17:54:47 +0530

    Displaymenu and Header added

---
+++ src/components/DisplayMenu/DisplayMenu.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import './DisplayMenu.css';
+import { useKanban } from '../../context/KanbanContext';
+import { GROUPING_OPTIONS, SORTING_OPTIONS } from '../../constants/index';
+
+function DisplayMenu({ onClose }) {
+  const { state, dispatch } = useKanban();
+
+  const handleGroupingChange = (value) => {
+    dispatch({ type: 'SET_GROUP_BY', payload: value });
+  };
+
+  const handleSortingChange = (value) => {
+    dispatch({ type: 'SET_SORT_BY', payload: value });
+  };
+
+  return (
+    <div className="display-menu">
+      <div className="menu-item">
+        <label>Grouping</label>
+        <select 
+          value={state.groupBy}
+          onChange={(e) => handleGroupingChange(e.target.value)}
+        >
+          {GROUPING_OPTIONS.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="menu-item">
+        <label>Ordering</label>
+        <select 
+          value={state.sortBy}
+          onChange={(e) => handleSortingChange(e.target.value)}
+        >
+          {SORTING_OPTIONS.map(option => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default DisplayMenu;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './Header.css';
+import DisplayMenu from '../DisplayMenu/DisplayMenu';
+import { SVG_ICONS } from '../../constants/icons';
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <header className="header">
+      <button 
+        className="display-button"
+        onClick={() => setShowMenu(!showMenu)}
+      >
+        <span className="display-icon">
+          <img src={SVG_ICONS.general.Display} alt="Display Icon" />
+        </span>
+        <span>Display</span>
+        <span className="arrow-icon">
+          <img src={SVG_ICONS.general.Down} alt="Down Arrow Icon" />
+        </span>
+      </button>
+      {showMenu && <DisplayMenu onClose={() => setShowMenu(false)} />}
+    </header>
+  );
+}
+
+export default Header;
